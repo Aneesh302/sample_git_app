af674a90cc8b22be4f855da6f0bb7d5ca2529d94
Aneesh302 <kalita.aneesh@gmail.com>
2025-03-18 10:37:00 +0530

    added conditional message

--- app.py
+++ app.py
@@ -24,5 +24,8 @@ st.sidebar.markdown("""
 - career
 - login""")
 
-st.sidebar.selectbox('Select one',['teacher','student'])
-st.sidebar.button('Select')
+option=st.sidebar.selectbox('Select one',['teacher','student'])
+btn=st.sidebar.button('Select')
+
+if btn:
+    st.title("Hello "+option)
